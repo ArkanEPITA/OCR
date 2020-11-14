7e85d8bbfdfaf4b6f9dfbfde565d7d4473166afa
lenoic.cramon <lenoic.cramon@epita.fr>
2020-11-14 18:15:46 +0100

    ajout d'un fichier xor avec moins de chose useless et modification des fonction de poid

--- xor/xor.c
+++ xor/xor.c
@@ -5,9 +5,9 @@
 
 
 
-//=========================================//
+//========================================//
 //                Structures               //
-//=========================================//
+//========================================//
 
 
 typedef struct Nodes Nodes;
@@ -25,7 +25,10 @@ struct Nodes
 
     double hiddenWeights[2][2]; //poid des couches caché
     double outputWeights[2][1]; //poid des couches de sortie
+    
     double deltaOutput[1];
+    double deltaHidden[2];
+    double Error;
     
     double lr;
 };
@@ -61,7 +64,7 @@ double dSigmoid(double z)		//dériver de la fonction d'activation
 	return res;
 }
 
-double init()   //fonction permettant d'initialiser mes poids de manière aléatoire
+/*double init()   //fonction permettant d'initialiser mes poids de manière aléatoire
 {
     double rnd;
     double a = 1.0;
@@ -73,48 +76,46 @@ double init()   //fonction permettant d'initialiser mes poids de manière aléat
     if(negative % 2 == 0)rnd = -rnd;
     return rnd;
 }
+*/
+double init()
+{
+	return (double) ((rand()%41)+30)/100;
+}
 
 void init_weight(Nodes* N)
 {
-    for (int input = 0; input < (*N).numInputs ; ++input)
+    for (int input = 0; input < 2 ; ++input)
     {
-        for (int hidden = 0; hidden < (*N).numHiddenNodes; ++hidden)
+        for (int hidden = 0; hidden < 2; ++hidden)
         {
             (*N).hiddenWeights[input][hidden] = init();
-            //(*N).hiddenWeights[input][hidden] = 0.0;
+            //(*N).hiddenWeights[input][0] = 0.0;
         }
-    }
-    for (int hidden = 0; hidden < (*N).numHiddenNodes; ++hidden)
-    {
-        for(int output = 0; output < (*N).numOutputs; ++output)
-        {
-            (*N).outputWeights[hidden][output] = init();
-            //(*N).outputWeights[hidden][output] = 0.0;
-        }
+        
+        (*N).outputWeights[input][0] = init();
+    	//(*N).outputWeights[hidden][0] = 0.0;
     }
 }
 
 void init_bias(Nodes* N)
 {
-    for (int i = 0; i < (*N).numHiddenNodes; ++i)
+    for (int i = 0; i < 2; ++i)
     {
         (*N).hiddenLayerBias[i] = init();
         //(*N).hiddenLayerBias[i] = 1.0f;
     }
-    for (int i = 0; i < (*N).numOutputs; ++i)
-    {
-        (*N).outputLayerBias[i] = init();
-        //(*N).outputLayerBias[i] = 1.0f;
-    }
+    
+    (*N).outputLayerBias[0] = init();
+    //(*N).outputLayerBias[0] = 1.0f;
 }
 
 void hidden_activation(Nodes* N, Training* T)
 {
-	for (int j = 0; j < (*N).numHiddenNodes; j++) //calcul de l'activation de la couche caché
+	for (int j = 0; j < 2; j++) //calcul de l'activation de la couche caché
 	{
 		double activation = (*N).hiddenLayerBias[j];
   	
-		for (int k = 0; k < (*N).numInputs; k++) 
+		for (int k = 0; k < 2; k++) 
 	   	{
 	   		activation += (*T).training_inputs[(*T).actTraining][k] * (*N).hiddenWeights[k][j];
 	   		
@@ -125,43 +126,37 @@ void hidden_activation(Nodes* N, Training* T)
 
 void output_activation(Nodes* N)
 {
-	for (int j = 0; j < (*N).numOutputs; j++) //calcul de l'activation de la couche de sortie
+	double activation = (*N).outputLayerBias[0];
+	
+	for (int k = 0; k < 2; k++) 
 	{
-		double activation = (*N).outputLayerBias[j];
-		
-		for (int k = 0; k < (*N).numHiddenNodes; k++) 
-		{
-			activation += (*N).hiddenLayer[k] * (*N).outputWeights[k][j];
-		}
-				
-		(*N).outputLayer[j] = sigmoid(activation);
-		//printf("layer :%lf\n", (*N).hiddenLayer[j]);
+		activation += (*N).hiddenLayer[k] * (*N).outputWeights[k][0];
 	}
+				
+	(*N).outputLayer[0] = sigmoid(activation);
+	//printf("layer :%lf\n", (*N).hiddenLayer[j]);
 }
-
+/*
 void modif_hidden_weight(Nodes* N, Training* T)
 {
 	double deltaHidden[(*N).numHiddenNodes];
 	
-	for (int j = 0; j < (*N).numHiddenNodes; j++) //calcul des modifications des poids des couches caché
+	for (int j = 0; j < 2; j++) //calcul des modifications des poids des couches caché
 	{
-		double dError;
+		double dError = 0.0f;
 		
-		for(int k = 0; k < (*N).numOutputs; k++) 
-		{
-			dError = (*N).deltaOutput[k] * (*N).outputWeights[j][k];
-		}
-			
+		dError += (*N).deltaOutput[0] * (*N).outputWeights[j][0];
+		
 		deltaHidden[j] = dError * dSigmoid((*N).hiddenLayer[j]);
 		//printf("derror : %lf\n", deltaHidden[j]);
 		//printf("delta hidden: %lf\n", deltaHidden[j]);
 	}
-	for (int j = 0; j < (*N).numHiddenNodes; j++) //Appliaction des modifications des poids des couches cachés
+	for (int j = 0; j < 2; j++) //Appliaction des modifications des poids des couches cachés
 	{
 		//(*N).hiddenLayerBias[j] += deltaHidden[j] * (*N).lr;
 		(*N).hiddenLayerBias[j] += deltaHidden[j];
 				
-		for(int k = 0; k < (*N).numInputs; k++) 
+		for(int k = 0; k < 2; k++) 
 		{
 			//(*N).hiddenWeights[k][j] += (*T).training_inputs[(*T).actTraining][k] * deltaHidden[j] * (*N).lr;
 			(*N).hiddenWeights[k][j] += (*T).training_inputs[(*T).actTraining][k] * deltaHidden[j];
@@ -169,28 +164,59 @@ void modif_hidden_weight(Nodes* N, Training* T)
 	}
 	
 }
-
+/*Je pense que l'erreur est la au dessus*/
+/*
 void modif_output_weight(Nodes* N, Training* T)
 {
 		
-	for (int j = 0; j < (*N).numOutputs; j++) //calcul des modifications des poids de sortie
+	double dError = ((*T).training_outputs[(*T).actTraining][0] - (*N).outputLayer[0]);
+	(*N).Error = dError;
+	(*N).deltaOutput[0] = dError * dSigmoid((*N).outputLayer[0]);
+	//printf("delta output: %lf\n", (*N).deltaOutput[j]);
+		
+	//(*N).outputLayerBias[j] += (*N).deltaOutput[j] * (*N).lr;
+	(*N).outputLayerBias[0] += (*N).deltaOutput[0];
+	for (int k = 0; k < 2; k++) 
 	{
-		double dError = ((*T).training_outputs[(*T).actTraining][j] - (*N).outputLayer[j]);
-		(*N).deltaOutput[j] = dError * dSigmoid((*N).outputLayer[j]);
-		//printf("delta output: %lf\n", (*N).deltaOutput[j]);
+		//(*N).outputWeights[k][j] += (*N).hiddenLayer[k] * (*N).deltaOutput[j] * (*N).lr;
+		(*N).outputWeights[k][0] += (*N).hiddenLayer[k] * (*N).deltaOutput[0];
+		//printf("delta output: %lf\n", (*N).hiddenLayer[k] * (*N).deltaOutput[j]);
+	}
+}
+*/
+
+void change_hidden_weight(Nodes* N)
+{
+	for(int i = 0; i < 2; i++)
+	{
+		for(int j = 0; j < 2; j++)
+		{
+			(*N).hiddenWeights[i][j] += (*N).deltaHidden[i];
+		}
+		
+		(*N).hiddenLayerBias[i] += (*N).deltaHidden[i];
 	}
 	
-	for (int j = 0; j < (*N).numOutputs; j++) //Appliaction des modifications des poids de sortie
+}
+
+void change_output_weight(Nodes* N)
+{
+	for(int i = 0; i < 2; i++)
 	{
-		//(*N).outputLayerBias[j] += (*N).deltaOutput[j] * (*N).lr;
-		(*N).outputLayerBias[j] += (*N).deltaOutput[j];
-		for (int k = 0; k < (*N).numHiddenNodes; k++) 
-		{
-			//(*N).outputWeights[k][j] += (*N).hiddenLayer[k] * (*N).deltaOutput[j] * (*N).lr;
-			(*N).outputWeights[k][j] += (*N).hiddenLayer[k] * (*N).deltaOutput[j];
-			//printf("delta output: %lf\n", (*N).hiddenLayer[k] * (*N).deltaOutput[j]);
-		}
+		(*N).outputWeights[i][0] += (*N).deltaOutput[0];
 	}
+	(*N).outputLayerBias[0] += (*N).deltaOutput[0];
+}
+
+void calcul_erreur_changement_poids(Nodes* N, Training* T)
+{
+	(*N).deltaOutput[0] = dSigmoid((*N).outputLayer[0]) * ((*T).training_outputs[(*T).actTraining][0] - (*N).outputLayer[0]);
+	change_output_weight(N);
+	for(int i = 0; i < 2; i++)
+	{
+		(*N).deltaHidden[i] = dSigmoid((*N).hiddenLayer[0]) * (*N).deltaOutput[0] * (*N).outputWeights[i][0];
+	}
+	change_hidden_weight(N);
 }
 
 
@@ -206,7 +232,7 @@ int print_xor(Training* T, Nodes* N, int epoch, int print)
 			printf("weight hidden 1 :%f\n", (*N).hiddenWeights[1][0]);
 			printf("weight hidden 2 :%f\n", (*N).hiddenWeights[1][1]);
 			printf("weight output 1 :%f\n", (*N).outputWeights[0][0]);
-			//printf("weight output 2 :%f\n", (*N).outputWeights[0][1]);
+			printf("weight output 2 :%f\n", (*N).outputWeights[0][1]);
 			printf("hidden bias 1:%f\n", (*N).hiddenLayerBias[0]);
 			printf("hidden bias 2:%f\n", (*N).hiddenLayerBias[1]);
 			printf("output bias :%f\n\n\n", (*N).outputLayerBias[0]);
@@ -219,11 +245,11 @@ int print_xor(Training* T, Nodes* N, int epoch, int print)
 
 void training(Nodes* N, Training* T)
 {
-
+/*
     for (int e = 0; e <= (*T).epochs; ++e)
     {
     	int print = 1;
-        for (int i = 0; i < (*T).numTrainingSets; ++i)
+        for (int i = 0; i < 4; ++i)
         {
         	(*T).actTraining = i;
             hidden_activation(N, T);
@@ -233,6 +259,24 @@ void training(Nodes* N, Training* T)
             print = print_xor(T, N, e, print);
         }
     }
+*/
+	int e = 0;
+	while(fabs((*N).Error) > 0.01)
+	{
+		int print = 1;
+        for (int i = 0; i < 4; ++i)
+        {
+        	(*T).actTraining = i;
+            hidden_activation(N, T);
+            output_activation(N);
+            //modif_output_weight(N, T);
+            //modif_hidden_weight(N, T);
+            calcul_erreur_changement_poids(N, T);
+            print = print_xor(T, N, e, print);
+        }
+        e += 1;
+	}
+	printf("nombre d'iteration : %d\n\n", e);
 }
 
 /*
@@ -263,6 +307,7 @@ void main()
     N.numHiddenNodes = 2;
     N.numOutputs = 1;
     N.lr = 0.1;
+    N.Error = 0.5;
     
     T.numTrainingSets = 4;
     T.training_inputs[0][0] = 0.0f;
@@ -278,7 +323,7 @@ void main()
     T.training_outputs[1][0] = 1.0f;
     T.training_outputs[2][0] = 1.0f;
     T.training_outputs[3][0] = 0.0f;
-    T.epochs = 2000;
+    T.epochs = 200000;
     T.actTraining = 0;
 
 //========================================//
